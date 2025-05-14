1c07c389c9f959bad5c93e449da1460d86360265
PPMGKKApplProgTeacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-05-14 10:39:18 +0300

    Create WhileLoops.cs

---
+++ WhileLoops.cs
@@ -0,0 +1,143 @@
+// Колко пъти ще се изпълни следващия цикъл и кое е последното изведено число?
+
+int i = 1;
+while (i < 10)
+{
+    Console.WriteLine(i);
+    i += 3;
+}
+
+int i = 10;
+while (i > 0)
+{
+    Console.WriteLine(i);
+    i -= 2;
+}
+
+int i = 0;
+while (i <= 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int i = -5;
+while (i < 0)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int i = 20;
+while (i >= 10)
+{
+    Console.WriteLine(i);
+    i -= 5;
+}
+
+int i = 2;
+while (i < 20)
+{
+    Console.WriteLine(i);
+    i *= 2;
+}
+
+int i = 50;
+while (i >= 25)
+{
+    Console.WriteLine(i);
+    i -= 10;
+}
+
+int i = 1;
+while (i < 100)
+{
+    Console.WriteLine(i);
+    i *= 3;
+}
+
+int i = 0;
+while (i != 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int i = 10;
+while (i <= 10)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int i = 3;
+while (i <= 30)
+{
+    Console.WriteLine(i);
+    i += 3;
+}
+
+int i = 15;
+while (i > 0)
+{
+    Console.WriteLine(i);
+    i -= 4;
+}
+
+int i = 100;
+while (i > 50)
+{
+    Console.WriteLine(i);
+    i -= 25;
+}
+
+int i = -10;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i += 3;
+}
+
+int i = 1;
+while (i <= 16)
+{
+    Console.WriteLine(i);
+    i *= 2;
+}
+
+int i = 7;
+while (i < 30)
+{
+    Console.WriteLine(i);
+    i += 7;
+}
+
+int i = 5;
+while (i > -5)
+{
+    Console.WriteLine(i);
+    i--;
+}
+
+int i = 9;
+while (i >= 0)
+{
+    Console.WriteLine(i);
+    i -= 3;
+}
+
+int i = 6;
+while (i <= 18)
+{
+    Console.WriteLine(i);
+    i += 6;
+}
+
+
+
+int i = 4;
+while (i < 20)
+{
+    Console.WriteLine(i);
+    i += 5;
+}
