02fbe2f5f595f92ba46b2516c16077cab22026a9
PPMGKKApplProgTeacher <ppmg_kk_appl_prog_teacher@proton.me>
2025-05-19 08:59:28 +0300

    Create stringbuilder.cs

---
+++ stringbuilder.cs
@@ -0,0 +1,61 @@
+// Използвайте StreamBuilder, за да промените променливата до желания резултат.  Опитайте да намерите по няколко решения на всеки пример
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "Hello"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "Wor"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "Hello\nWorld\n"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда "Hello, World"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда "Hello World"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда "Hello Universe"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда "Hello"
+
+var str = new StringBuilder("Hello");
+Console.WriteLine(str.Length); // Извежда 5
+
+var str = new StringBuilder("Hello");
+Console.WriteLine(str.ToString()); // Извежда "Hello"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда празен низ
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "123"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "*"
+
+var str = new StringBuilder("World");
+Console.WriteLine(str); // Извежда "Hello World"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "Hello World"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "01234"
+
+var str = new StringBuilder();
+Console.WriteLine(str); // Извежда "Line 1\nLine 2\n"
+
+var str = new StringBuilder("HelloWorld");
+Console.WriteLine(str); // Извежда "Hello\nWorld"
+
+var str = new StringBuilder("Hello World");
+Console.WriteLine(str); // Извежда "Hello_World"
+
+var str = new StringBuilder("HELLO WORLD");
+Console.WriteLine(str); // Извежда "hello world"
+
+var str = new StringBuilder("hello world");
+Console.WriteLine(str); // Извежда "HELLO WORLD"
